d681df62a3d5d991caf63862800e16cbc30d55df
zhoufeng <zhoufeng@everchain.info>
2021-08-18 21:52:18 +0800

    day7

--- Algorithm/Algorithm/ViewController.swift
+++ Algorithm/Algorithm/ViewController.swift
@@ -17,8 +17,16 @@ class ViewController: UIViewController {
         var arr = [[1,2,3],[4,5,6],[7,8,9]]
         let r = u.generateMatrix(3)
         print(r)
+        
+        test()
     }
 
-
+    func test() {
+        let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(callBack), userInfo: nil, repeats: true)
+    }
+    
+    @objc func callBack() {
+        print(123)
+    }
 }
 
